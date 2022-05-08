bf5033953301ec1722741b820ab77006cb0ef9ae
fatalem0 <madchaterrer@gmail.com>
2022-05-08 21:44:21 +0300

    Initial commit

---
+++ src/main/java/com/fatalem0/SpringWeatherClient/service/WeatherService.java
@@ -0,0 +1,11 @@
+package com.fatalem0.SpringWeatherClient.service;
+
+import org.springframework.web.client.RestTemplate;
+
+public class WeatherService {
+    private RestTemplate restTemplate;
+
+    public Weather getWeather() {
+        return null;
+    }
+}
